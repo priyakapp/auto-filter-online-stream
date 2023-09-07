b24b7dc01b8b4fec6061cf7d3ee2b6b25a3deb77
priyakapp <84661064+priyakapp@users.noreply.github.com>
2023-09-07 13:54:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
-git clone https://github.com/priyakapp/auto-filter-online-stream /THALAPATHY-FILTER-BOT
-cd /THALAPATHY-FILTER-BOT
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/1238muj/THALAPATHY-FILTER-BOT.git /THALAPATHY-FILTER-BOT 
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /THALAPATHY-FILTER-BOT 
+fi
+cd /THALAPATHY-FILTER-BOT 
 pip3 install -U -r requirements.txt
-echo "starting bot...."
+echo "Starting Bot...."
 python3 bot.py

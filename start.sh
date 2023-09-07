30632d34e0abdfb8de2b1242724d0495585b43ad
priyakapp <84661064+priyakapp@users.noreply.github.com>
2023-09-07 12:16:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git clone https://github.com/priyakapp/auto-filter-online-stream /THALAPATHY-FILTER-BOT
-
 cd /THALAPATHY-FILTER-BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot..."
